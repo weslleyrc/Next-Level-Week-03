9e2c6f919eb08f04e2270e433a55052e14f34e2f
Weslley.rangel <Weslley-rangel@outlook.com>
2020-10-19 20:04:23 -0300

    criaçao da galeria de imagens, feita alteracao na pagina orphanage.html, para buscar imagens de uma galeria virtual e no index.html para redirecionar o endereço para orphanages.html

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -29,6 +29,25 @@ L.marker([-22.9539601,-43.2990787], { icon })
 /*image gallery*/
 
 function selectImage(event){
-    console.log("cliquei no botao")
+    const button = event.currentTarget
+
+    //remover todas as classes .active
+    const buttons = document.querySelectorAll(".images button");
+    buttons.forEach(removeActiveClass) //forEach() = para cada
+    
+    function removeActiveClass(button){
+        button.classList.remove("active")
+    }
+    
+
+    //selecionar a imagem clicada
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+    // atualizar o container de image
+    imageContainer.src = image.src
+
+    // adicionar a classe .active para o botao clicado
+    button.classList.add('active')
 }
 
