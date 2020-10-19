d6e76e83a920f036efef08bd4ca8e03e27c1a961
Weslley.rangel <Weslley-rangel@outlook.com>
2020-10-19 18:08:04 -0300

    criada pagina orphanage.html, adicionado css page-orphanage.css, criado arquivo JS page-orphanage.js. ajuste do mapa no js

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,28 @@
+const options = { /*Desabilitando função de Zoom do mapa*/
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false, 
+}
+
+//create map
+const map = L.map('mapid', options).setView([-22.9539601,-43.2990787], 15);
+
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+
+//create and add marker
+L.marker([-22.9539601,-43.2990787], { icon })
+    .addTo(map)
+    
