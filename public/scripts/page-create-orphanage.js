77b7a2dcb7230321a7ba122ce3740982d404e086
Weslley.rangel <Weslley-rangel@outlook.com>
2020-10-21 22:33:34 -0300

    testando o terminal do visual code

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -84,9 +84,12 @@ function toggleSelect(event){
     const button = event.currentTarget
     button.classList.add('active')
 
-    //verificar se sim ou nao
-
     //atualizar o meu input hidden com o valor selecionado
+    const input = document.querySelector('[name="open_on_weekends"]')
+    
+    input.value = button.dataset.value
+    // verificar se sim ou nao
+
 
     //retirar a class .active
 
